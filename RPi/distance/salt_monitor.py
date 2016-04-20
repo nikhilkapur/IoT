f1b4e1052fef2acbd3c7e3fe1349b29c522b9cc9
Nikhil Kapur (salt-monitor) <nikhil@kapurs.net>
2016-04-20 14:33:35 -0700

    Added support for notifications

--- RPi/distance/salt_monitor.py
+++ RPi/distance/salt_monitor.py
@@ -1,23 +1,44 @@
 
-#import ultra_sonic
+import ultra_sonic
 import datetime
 #import time
 import MySQLdb
 import random
+import os
 
 def check_level():
-    trig_pin = 20                                 #Associate pin 23 to trig_pin
-    echo_pin = 21                                  #Associate pin 24 to echo_pin
-    #dm = ultra_sonic.DistanceMeasurer(trig_pin, echo_pin)
-    #dist = dm.get_dist()
-    #return dist
-    return random.randrange(2,400)
+    trig_pin = 18                                 #Associate pin 23 to trig_pin
+    echo_pin = 23                                  #Associate pin 24 to echo_pin
+    dm = ultra_sonic.DistanceMeasurer(trig_pin, echo_pin)
+    dist = dm.get_dist()
+    return dist
+    #return random.randrange(2,400)
 
 def send_notification(level):
-    pass
+
+    sender = 'salt-monitor@pizero'
+    recipients = ['nikhil@kapurs.net']
+    subject = "Salt Level Low [%d]" % level
+    text = "http://home.kapurs.net:11680/cgi-bin/salt-monitor/salt_chart.py?days=60&show_raw=0"
+        
+    status = send_mail (sender, recipients, subject, text)
+
+
+def send_mail(sender, recipients, subject, text):
+    message = """From: %s\nTo: %s\nSubject: %s
+
+    %s 
+    """ % (sender, ", ".join(recipients), subject, text)
+
+    p = os.popen("/usr/sbin/sendmail -t -i", "w")
+    p.write(message)
+    status = p.close()
+    return status
+
+
 
 def save_data(level):
-    db = MySQLdb.connect(host="localhost", user="root", passwd="nkapur", db="salt_monitor")
+    db = MySQLdb.connect(host="localhost", user="salt_monitor", passwd="salt_monitor123", db="salt_monitor")
     cur = db.cursor()
     cur.execute("INSERT INTO salt_level (sl_level, sl_date) VALUES (%s, %s)", (level, datetime.datetime.today()))
     db.commit()
@@ -28,7 +49,8 @@ def save_data(level):
 
 if __name__ == "__main__":
     level = check_level()
-    if level < 300:
+    if level > 60:
         send_notification (level)
     save_data(level)
+    print level
     
