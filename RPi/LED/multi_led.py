c2218994f5f0dcff72b8d605f0aea4943a66483d
Nikhil Kapur <nkapur@cisco.com>
2016-04-21 10:33:23 -0700

    Added PosSinWave Optimized code

--- RPi/LED/multi_led.py
+++ RPi/LED/multi_led.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import math
 
 
 class RGBled:
@@ -21,45 +22,66 @@ class RGBled:
         self.green_pwm = GPIO.PWM (green, hz)
         self.blue_pwm  = GPIO.PWM (blue,  hz)
 
-    def change_colors(self, speed = 0.01):
-        # Start PWM with duty cycle 0 and then change DC in a loop to get the effect
+        # Start PWM with duty cycle 0 (off)
         self.blue_pwm.start(0)
         self.green_pwm.start(0)
         self.red_pwm.start(0)
-        
-        for i in range(100):
-            self.green_pwm.ChangeDutyCycle(100-i)
-            self.blue_pwm.ChangeDutyCycle(i)
-            time.sleep (speed)
 
-        self.blue_pwm.start(0)
-        self.green_pwm.start(0)
-        self.red_pwm.start(0)
-        #time.sleep(1)
-
-        for i in range(100):
-            self.blue_pwm.ChangeDutyCycle(100-i)
-            self.red_pwm.ChangeDutyCycle(i)
-            time.sleep (speed)
-
-        self.blue_pwm.start(0)
-        self.green_pwm.start(0)
-        self.red_pwm.start(0)
-        #time.sleep(1)
-
-        for i in range(100):
-            self.red_pwm.ChangeDutyCycle(100-i)
-            self.green_pwm.ChangeDutyCycle(i/2)
-            time.sleep (speed)
-
+    def __del__(self):
         self.blue_pwm.stop()
         self.green_pwm.stop()
         self.red_pwm.stop()
 
+    
+    def change_colors(self, dc_r, dc_g, dc_b, delay=0):
+        '''Change the duty cycle of each LED's PWM to values specified
+            If a value is None, the DC will remain unchanged
+        '''
+        
+        if dc_r != None : self.red_pwm.ChangeDutyCycle(dc_r) 
+        if dc_g != None : self.green_pwm.ChangeDutyCycle(dc_g)
+        if dc_b != None : self.blue_pwm.ChangeDutyCycle(dc_b)
+        
+        time.sleep (delay)
+
+    def cycle_colors(self, delay=0.01):
+        '''Cycle through different color combinations'''
+        for i in range(100):
+            self.change_colors(None, 100-i, i, delay=delay)
+
+        for i in range(100):
+            self.change_colors(i, None, 100-i, delay=delay)
+
+        for i in range(100):
+            self.change_colors(100-i, i/2, None, delay=delay)
+
+
+    def PosSinWave(self, amplitude, angle, frequency):
+        ''' Adapted from http://www.henryleach.com/2013/05/controlling-rgb-led-with-raspberry-pi.html
+        '''
+        #angle in degrees  
+        #creates a positive sin wave between 0 and amplitude*2  
+        return amplitude + (amplitude * math.sin(math.radians(angle)*frequency) )  
+
+    def change_colors_sin(self):
+        try:  
+            while 1:  
+                for i in range(0, 720, 5):  
+                    self.change_colour( self.PosSinWave(50, i, 0.5),  
+                                        self.PosSinWave(50, i, 1),  
+                                        self.PosSinWave(50, i, 2),  
+                                        delay = 0.1 )  
+       
+        except KeyboardInterrupt:  
+            pass  
+
 if __name__ == "__main__":
     GPIO.setmode (GPIO.BCM)
+    
     led = RGBled(23, 18, 25)
+    
     for i in range(5):
-        led.change_colors()
+        led.cycle_colors(delay=0.01)
 
+    led.change_colors_sin()
     GPIO.cleanup()
