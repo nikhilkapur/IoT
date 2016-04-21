55fd957fa97e5be47c69fe47abce65ac7f75347e
Nikhil Kapur <nkapur@cisco.com>
2016-04-20 17:07:09 -0700

    Dont call GPIO.cleanup() to avoid interference with other modules Make setmode more generic

--- RPi/distance/ultra_sonic.py
+++ RPi/distance/ultra_sonic.py
@@ -5,19 +5,21 @@ import time                                #Import time library
 
 class DistanceMeasurer:
     
-    def __init__(self, trig_pin, echo_pin, settle_time=1, range_min=2, range_max=500):
+    def __init__(self, trig_pin, echo_pin, settle_time=1, range_min=2, range_max=500, mode=GPIO.BCM):
         self.trig_pin = trig_pin
         self.echo_pin = echo_pin
         self.settle_time = settle_time
         self.range_min = range_min
         self.range_max = range_max
         
-        GPIO.setmode(GPIO.BCM)                          #Set GPIO pin numbering 
+        GPIO.setmode(mode)                          #Set GPIO pin numbering 
         GPIO.setup(trig_pin, GPIO.OUT)                  #Set pin as GPIO out
         GPIO.setup(echo_pin, GPIO.IN)                   #Set pin as GPIO in
 
-    def __del__(self):
-        GPIO.cleanup()
+    # This may not be a good idea as the object could go out of scope while the rest of the calling program
+    # is still using GPIO port. Better handled by the calling program
+    #def __del__(self):
+    #    GPIO.cleanup()
 
     def get_dist(self):
         GPIO.output(self.trig_pin, False)               #Set trig_pin as LOW
@@ -48,6 +50,7 @@ class DistanceMeasurer:
 ##############################################################################################################
 
 if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
     trig_pin = 18                                       #Associate pin 18 to trig_pin
     echo_pin = 23                                       #Associate pin 23 to echo_pin
     dm = DistanceMeasurer(trig_pin, echo_pin)
@@ -58,7 +61,8 @@ if __name__ == "__main__":
             print 'Distance is %f' % dist
         except RuntimeError, e:
             print 'ERR Distance is %s' % e
-            
+         
+    GPIO.cleanup()   
         
 
 
