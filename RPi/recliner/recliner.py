d9c810a48eafebe5d012dc6f1f04067893af64e2
nkapur <nkapur@nkapur-m-208q>
2017-07-11 20:41:32 -0700

    Converted to class

--- RPi/recliner/recliner.py
+++ RPi/recliner/recliner.py
@@ -1,34 +1,56 @@
 #!/usr/bin/python
 
-print "hello world"
+from __future__ import division
 
-import RPi.GPIO as GPIO  
+#import RPi.GPIO as GPIO
+import gpio as GPIO
+  
 import time
 
 class recliner:
-    def init(self
-def up(percent=100):
-    print "low"
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(delay)
-    print "high"
-    GPIO.output(pin, GPIO.HIGH)
-    #time.sleep(delay)
-
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
-
-for pin in pins:
-    print pin
-    toggle_led(pin, delay=8)
+    def init(self, up_pin=17, down_pin=27, on_level=GPIO.LOW, gpio_mode=GPIO.BCM, recline_time=10):
+        GPIO.setmode(gpio_mode)
+        self.recline_time = recline_time
+        self.up_pin = up_pin
+        self.down_pin = down_pin
+        if on_level == GPIO.LOW:
+            self.on_level  = GPIO.LOW
+            self.off_level = GPIO.HIGH
+        else:
+            self.on_level  = GPIO.HIGH
+            self.off_level = GPIO.LOW
+        self.reset()
     
-GPIO.cleanup()
+    def reset(self):
+        GPIO.output(self.up_pin, self.off_level) 
+        GPIO.output(self.down_pin, self.off_level) 
+    
+    def up(self, percent=100):
+        duration = int(self.recline_time * percent/100 +0.5)
+        GPIO.output(self.up_pin, self.on_level)
+        time.sleep(duration)
+        GPIO.output(self.up_pin, self.off_level)
 
+    def down(self, percent=100):
+        duration = int(self.recline_time * percent/100 +0.5)
+        GPIO.output(self.down_pin, self.on_level)
+        time.sleep(duration)
+        GPIO.output(self.down_pin, self.off_level)
+             
+    
 if __name__ == "__main__":
-    # Use BCM numbering
-    GPIO.setmode(GPIO.BCM)
-    
-    #pins = [2,3,18,23]
-    pins = [17,27]
+    r = recliner (up_pin=17, down_pin=27)
+    r.up()
+    time.sleep(2)
+    r.down()
+    time.sleep(2)
+
+    r.up(50)
+    time.sleep(2)
+    r.down(50)
+    time.sleep(2)
+
+    GPIO.cleanup()
+
      
                 
